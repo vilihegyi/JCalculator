dbf523db41b528e0689d0be9c4fe7d51d2e988eb
Vili <vilihegyi@gmail.com>
2018-01-13 19:48:38 +0200

    Modifying Console into a Main class
    Added GUI, replacing the console version with a Main class that instantiates the GUI for the program.

--- src/Main.java
+++ src/Main.java
@@ -1,79 +1,81 @@
-package calc;
+package program;
 
-import java.util.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 public class Main {
   
-  //initalizing the Scanner to read data from the user
-	static final Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
+		JMenuBar menuBar;
+		JMenu menu;
+		JMenuItem simpleView, scientificView, programmingView, about;
 
-	public static void main(String[] args) {
-    //We store the first value in result, and we will work with that. It will be updated as operations are done to it
-		float result = 0, new_number;
-		String op;
-		System.out.print("Introduce the first number: ");
-		new_number = input.nextFloat();
-		result += new_number;
-		while (true) {
-			System.out.print("Introduce the operation: ");
-			op = input.next();
-      //Different cases for different operations, along with those being made once the next number is introduced as well
-			switch (op) {
-			case "CE": {
-				result = 0;
-				System.out.print("Introduce the new first number: ");
-				result = input.nextFloat();
-				break;
+	  	// Creating objects of type Class to be able to use the methods within them
+		SimpleCalculator simpleCalculator = new SimpleCalculator();
+		ScientificCalculator scientificCalculator = new ScientificCalculator();
+
+		// Create the menu bar
+		menuBar = new JMenuBar();
+		// Creating a menu
+		menu = new JMenu("Switch View");
+		// Adding the menu to the menuBar
+		menuBar.add(menu);
+		// Creating items for the menu and adding them to the menu
+		// Simple
+		simpleView = new JMenuItem("Simple Calculator");
+		menu.add(simpleView);
+		// Math / Scientific
+		scientificView = new JMenuItem("Scientific Calculator");
+		menu.add(scientificView);
+		// Separator
+		menu.addSeparator();
+		// About - separated
+		about = new JMenuItem("About");
+		menu.add(about);
+
+		simpleCalculator.setVisible(true);
+		simpleCalculator.setJMenuBar(menuBar);
+
+		// Hiding the other view
+		scientificCalculator.setVisible(false);
+
+		// ActionListeners to the JMenu items
+		simpleView.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+
+				// Closing any previous windows
+				scientificCalculator.setVisible(false);
+
+				// Adding the existing menu to the new frame
+				simpleCalculator.setVisible(true);
+				simpleCalculator.setJMenuBar(menuBar);
 			}
-			case "*": {
-				System.out.print("Introduce the next number: ");
-				new_number = input.nextFloat();
-				System.out.print(result + " * " + new_number + " = ");
-				result *= new_number;
-				System.out.println(result);
-				break;
+		});
+
+		scientificView.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+
+				// Closing any previous windows
+				simpleCalculator.setVisible(false);
+
+				// Adding the existing menu to the new frame
+				scientificCalculator.setVisible(true);
+				scientificCalculator.setJMenuBar(menuBar);
 			}
-			case "/": {
-				System.out.print("Introduce the next number: ");
-				new_number = input.nextFloat();
-				System.out.print(result + " / " + new_number + " = ");
-				result /= new_number;
-				System.out.println(result);
-				break;
+		});
+
+		about.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				//Message dialog pops up with details about hte program
+				JOptionPane.showMessageDialog(null, "JCalculator is a multi-functional calculator program\nwritten in the Java programming language.\n\n                        Version 1.0");
 			}
-			case "%": {
-				System.out.print("Introduce the next number: ");
-				new_number = input.nextFloat();
-				System.out.print(result + " % " + new_number + " = ");
-				result %= new_number;
-				System.out.println(result);
-				break;
-			}
-			case "+": {
-				System.out.print("Introduce the next number: ");
-				new_number = input.nextFloat();
-				System.out.print(result + " + " + new_number + " = ");
-				result += new_number;
-				System.out.println(result);
-				break;
-			}
-			case "-": {
-				System.out.print("Introduce the next number: ");
-				new_number = input.nextFloat();
-				System.out.print(result + " - " + new_number + " = ");
-				result -= new_number;
-				System.out.println(result);
-				break;
-			}
-      /*In case an operation isn't listed, or is invalid, the program will exit
-      *(obviously for safety reasons, A.K.A. my laziness doesn't want me to develop the application any further!)
-      */
-			default: {
-				System.out.print("Invalid operation! The program will now exit!");
-				System.exit(0);
-				break;
-			}
-			}
-		}
+		});
 	}
 }
