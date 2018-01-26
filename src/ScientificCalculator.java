27f32c5d9438ee0b70aa665b20c8acea74d803b1
Dev <vilihegyi@gmail.com>
2018-01-26 13:38:56 +0200

    Update and comments
    Updated the file to fix some bugs and commented the code

--- src/ScientificCalculator.java
+++ src/ScientificCalculator.java
@@ -12,27 +12,49 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 public class ScientificCalculator extends JFrame {
+	// Declaration of a personal serialVersioUID not to have errors in the
+	// future
 	private static final long serialVersionUID = 128609292020710218L;
+	// Declaration of the buttons
 	private JButton zero, one, two, three, four, five, six, seven, eight, nine,
 			dot, clear, factorial, power, squareRoot, modulus, sin, cos, tan,
 			asin, acos, atan, sinh, cosh, tanh, changeSign;
+	// Declaration of the text area/screen
 	private JTextArea text;
+	// Declaration of a panel which holds our buttons and shows them on the
+	// frame
 	private JPanel panel;
+	// Declaration and creation of a custom size
+	private Dimension dim = new Dimension(75, 25);
+	// Declaration and creationg of a custom font style for the text appearance
+	private Font font = new Font("Serif", Font.ITALIC, 14);
+	// Variable of type double (for better precision in comparison to float) to
+	// work with at the trigonometrical functions
+	double num;
+	// String that holds the result and number on screen, as this calculator
+	// works with mono operational numbers
 	private String result = "";
+	// Boolean to check if the number is a non integer
 	private boolean hasDot = false;
 
+	// Class constructor
 	public ScientificCalculator() {
 		initUI();
 	}
 
+	// Method to initialize the interface
 	private void initUI() {
 
+		// JFrame specifications
 		setTitle("JCalculator");
 		setResizable(false);
+		// Creation of the panel
 		panel = new JPanel(new FlowLayout());
 
-		// 1 line, up to a total of 21 characters
+		// Text area: 1 line, up to a total of 21 characters
 		text = new JTextArea(1, 21);
+		// Creation of the buttons using "keyboard" characters, respectively
+		// UNICODE characters
 		zero = new JButton("0");
 		one = new JButton("1");
 		two = new JButton("2");
@@ -60,43 +82,47 @@ public class ScientificCalculator extends JFrame {
 		tanh = new JButton("tanh");
 		changeSign = new JButton("\u00B1");
 
-		sin.setFont(new Font("Serif", Font.ITALIC, 14));
-		cos.setFont(new Font("Serif", Font.ITALIC, 14));
-		tan.setFont(new Font("Serif", Font.ITALIC, 14));
-		asin.setFont(new Font("Serif", Font.ITALIC, 14));
-		acos.setFont(new Font("Serif", Font.ITALIC, 14));
-		atan.setFont(new Font("Serif", Font.ITALIC, 14));
-		sinh.setFont(new Font("Serif", Font.ITALIC, 14));
-		cosh.setFont(new Font("Serif", Font.ITALIC, 14));
-		tanh.setFont(new Font("Serif", Font.ITALIC, 14));
+		// Setting a custom font for the trigonometrical operations
+		sin.setFont(font);
+		cos.setFont(font);
+		tan.setFont(font);
+		asin.setFont(font);
+		acos.setFont(font);
+		atan.setFont(font);
+		sinh.setFont(font);
+		cosh.setFont(font);
+		tanh.setFont(font);
 
-		one.setPreferredSize(new Dimension(75, 25));
-		two.setPreferredSize(new Dimension(75, 25));
-		three.setPreferredSize(new Dimension(75, 25));
-		four.setPreferredSize(new Dimension(75, 25));
-		five.setPreferredSize(new Dimension(75, 25));
-		six.setPreferredSize(new Dimension(75, 25));
-		seven.setPreferredSize(new Dimension(75, 25));
-		eight.setPreferredSize(new Dimension(75, 25));
-		nine.setPreferredSize(new Dimension(75, 25));
-		zero.setPreferredSize(new Dimension(75, 25));
-		modulus.setPreferredSize(new Dimension(75, 25));
-		dot.setPreferredSize(new Dimension(75, 25));
+		// Setting a custom size for the buttons and scren
+		text.setPreferredSize(dim);
+		one.setPreferredSize(dim);
+		two.setPreferredSize(dim);
+		three.setPreferredSize(dim);
+		four.setPreferredSize(dim);
+		five.setPreferredSize(dim);
+		six.setPreferredSize(dim);
+		seven.setPreferredSize(dim);
+		eight.setPreferredSize(dim);
+		nine.setPreferredSize(dim);
+		zero.setPreferredSize(dim);
+		modulus.setPreferredSize(dim);
+		dot.setPreferredSize(dim);
 		clear.setPreferredSize(new Dimension(235, 25));
-		factorial.setPreferredSize(new Dimension(75, 25));
-		power.setPreferredSize(new Dimension(75, 25));
-		squareRoot.setPreferredSize(new Dimension(75, 25));
-		sin.setPreferredSize(new Dimension(75, 25));
-		cos.setPreferredSize(new Dimension(75, 25));
-		tan.setPreferredSize(new Dimension(75, 25));
-		asin.setPreferredSize(new Dimension(75, 25));
-		acos.setPreferredSize(new Dimension(75, 25));
-		atan.setPreferredSize(new Dimension(75, 25));
-		sinh.setPreferredSize(new Dimension(75, 25));
-		cosh.setPreferredSize(new Dimension(75, 25));
-		tanh.setPreferredSize(new Dimension(75, 25));
+		factorial.setPreferredSize(dim);
+		power.setPreferredSize(dim);
+		squareRoot.setPreferredSize(dim);
+		sin.setPreferredSize(dim);
+		cos.setPreferredSize(dim);
+		tan.setPreferredSize(dim);
+		asin.setPreferredSize(dim);
+		acos.setPreferredSize(dim);
+		atan.setPreferredSize(dim);
+		sinh.setPreferredSize(dim);
+		cosh.setPreferredSize(dim);
+		tanh.setPreferredSize(dim);
 		changeSign.setPreferredSize(new Dimension(235, 25));
 
+		// Adding the elements to the panel
 		panel.add(text);
 		panel.add(clear);
 		panel.add(seven);
@@ -125,12 +151,16 @@ public class ScientificCalculator extends JFrame {
 		panel.add(tanh);
 		panel.add(changeSign);
 
+		// Adding the panel to the frame
 		add(panel);
+
+		// Configuring the frame a little more
 		// width, height
-		setSize(250, 400);
+		setSize(250, 380);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		// Action handlers for the buttons
 		one.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent event) {
@@ -225,7 +255,7 @@ public class ScientificCalculator extends JFrame {
 				if (result.equals("0"))
 					result = "";
 				text.setText("");
-				result += "4";
+				result += "9";
 				text.setText(result);
 			}
 		});
@@ -246,6 +276,7 @@ public class ScientificCalculator extends JFrame {
 			public void actionPerformed(ActionEvent event) {
 				text.setText("");
 				result = "";
+				hasDot = false;
 			}
 		});
 
@@ -254,6 +285,8 @@ public class ScientificCalculator extends JFrame {
 			public void actionPerformed(ActionEvent event) {
 				if (!hasDot) {
 					hasDot = true;
+					if(result.equals(""))
+						result = "0";
 					result += ".";
 					text.setText(result);
 				}
@@ -263,20 +296,34 @@ public class ScientificCalculator extends JFrame {
 		factorial.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent event) {
-				int fact = 1, limit, i;
-				limit = Integer.parseInt(result);
-				for (i = 2; i <= limit; i++)
-					fact *= i;
+				int i;
+				double fact = 1;
+				
+					// In the for (due to laziness) we convert the number into
+					// an
+					// integer before performing the operation (this in case the
+					// number on the screen is of a non-integer type)
+					for (i = 2; i <= Integer.parseInt(result.split("\\.")[0]); i++)
+						fact *= i;
 
-				result = Integer.toString(fact);
-				text.setText(result);
+					if (fact != Double.POSITIVE_INFINITY) {
+						result = Double.toString(fact);
+						text.setText(result);
+					}
+					else {
+						result = "0";
+						text.setText("Number too large! Result is now 0!");
+					}
+				/*} catch (NumberFormatException e) {
+					text.setText("Number too large! Result will reset");
+					result = "";
+				}*/
 			}
 		});
 
 		power.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent event) {
-				double num;
 				num = Double.parseDouble(result);
 				num = Math.pow(num, 2);
 				result = Double.toString(num);
@@ -287,7 +334,6 @@ public class ScientificCalculator extends JFrame {
 		squareRoot.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent event) {
-				double num;
 				num = Double.parseDouble(result);
 				num = Math.sqrt(num);
 				result = Double.toString(num);
@@ -298,7 +344,6 @@ public class ScientificCalculator extends JFrame {
 		modulus.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent event) {
-				double num;
 				num = Double.parseDouble(result);
 				if (num < 0)
 					num *= -1;
@@ -307,114 +352,167 @@ public class ScientificCalculator extends JFrame {
 			}
 		});
 
-		sin.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				double num;
-				num = Double.parseDouble(result);
-				num = Math.sin(num);
-				result = Double.toString(num);
-				text.setText(result);
-			}
-		});
-
-		cos.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				double num;
-				num = Double.parseDouble(result);
-				num = Math.cos(num);
-				result = Double.toString(num);
-				text.setText(result);
-			}
-		});
-
-		tan.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				double num;
-				num = Double.parseDouble(result);
-				num = Math.tan(num);
-				result = Double.toString(num);
-				text.setText(result);
-			}
-		});
-		
-		asin.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				double num;
-				num = Double.parseDouble(result);
-				num = Math.asin(num);
-				result = Double.toString(num);
-				text.setText(result);
-			}
-		});
-
-		acos.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				double num;
-				num = Double.parseDouble(result);
-				num = Math.acos(num);
-				result = Double.toString(num);
-				text.setText(result);
-			}
-		});
-
-		atan.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				double num;
-				num = Double.parseDouble(result);
-				num = Math.atan(num);
-				result = Double.toString(num);
-				text.setText(result);
-			}
-		});
-		
-		sinh.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				double num;
-				num = Double.parseDouble(result);
-				num = Math.sinh(num);
-				result = Double.toString(num);
-				text.setText(result);
-			}
-		});
-
-		cosh.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				double num;
-				num = Double.parseDouble(result);
-				num = Math.cosh(num);
-				result = Double.toString(num);
-				text.setText(result);
-			}
-		});
-
-		tanh.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				double num;
-				num = Double.parseDouble(result);
-				num = Math.tanh(num);
-				result = Double.toString(num);
-				text.setText(result);
-			}
-		});
-
 		changeSign.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent event) {
-				double num;
 				num = Double.parseDouble(result);
 				num *= -1;
 				result = Double.toString(num);
 				text.setText(result);
 			}
 		});
+
+		sin.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				trigonometricalOperations('s', 0);
+			}
+		});
+
+		asin.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				trigonometricalOperations('s', 1);
+			}
+		});
+
+		sinh.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				trigonometricalOperations('s', 2);
+			}
+		});
+
+		cos.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				trigonometricalOperations('c', 0);
+			}
+		});
+
+		acos.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				trigonometricalOperations('c', 1);
+			}
+		});
+
+		cosh.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				trigonometricalOperations('c', 2);
+			}
+		});
+
+		tan.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				trigonometricalOperations('t', 0);
+			}
+		});
+
+		atan.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				trigonometricalOperations('t', 1);
+			}
+		});
+
+		tanh.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				trigonometricalOperations('t', 2);
+			}
+		});
+	}
+
+	// Method that handles the trigonometrical operations, to avoid code
+	// repitition
+	private void trigonometricalOperations(char trigonometricalOperation,
+			int type) {
+		num = Double.parseDouble(result);
+		// Based on the given operation (sin, cos, tan) and type (normal,
+		// inverse, hyperbolic) we do the operation
+		switch (trigonometricalOperation) {
+		case 's': {
+			switch (type) {
+			case 0: {
+				result = Double.toString(Math.sin(num));
+				break;
+			}
+
+			case 1: {
+				result = Double.toString(Math.asin(num));
+				break;
+			}
+
+			case 2: {
+				result = Double.toString(Math.sinh(num));
+				break;
+			}
+			// This will never be reached, but we have to put it, just in
+			// case...
+			default:
+				break;
+			}
+		}
+		case 'c': {
+			switch (type) {
+			case 0: {
+				result = Double.toString(Math.cos(num));
+				break;
+			}
+
+			case 1: {
+				result = Double.toString(Math.acos(num));
+				break;
+			}
+
+			case 2: {
+				result = Double.toString(Math.cosh(num));
+				break;
+			}
+			// This will never be reached, but we have to put it, just in
+			// case...
+			default:
+				break;
+			}
+		}
+		case 't': {
+			switch (type) {
+			case 0: {
+				result = Double.toString(Math.tan(num));
+				break;
+			}
+
+			case 1: {
+				result = Double.toString(Math.atan(num));
+				break;
+			}
+
+			case 2: {
+				result = Double.toString(Math.tanh(num));
+				break;
+			}
+			// This will never be reached, but we have to put it, just in
+			// case...
+			default:
+				break;
+			}
+		}
+		// This will never be reached, but we have to put it, just in case...
+		default:
+			break;
+		}
+
+		// Eventually, we show the result we've obtained
+		text.setText(result);
+	}
+
+	// In case we reopen
+	public void reset() {
+		text.setText("");
+		result = "";
+		hasDot = false;
 	}
 }
